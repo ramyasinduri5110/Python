a98f0af83d6715e52f27df10e5ee3205001975c2
ramyasinduri5110 <78115587+ramyasinduri5110@users.noreply.github.com>
2021-01-28 13:46:29 -0600

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,20 @@
+#This program is to know about the fundemental operations
+print("Addition:")
+print(8+4)
+print("Subtraction:")
+print(92-2)
+print("Multiplication:")
+print(10*7)
+print("Division:")
+print(9/3)
+print("Power")
+print(2**4)
+
+#operator precedence 
+#()
+#**
+#/
+#*
+#+
+#-
+print(20-3*4)
